a1a11f13dbbccb7ab6db63adc390864c090527a6
vrusali <101049802+vrusali@users.noreply.github.com>
2022-07-04 14:30:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+//3]AREA OF CIRCULE AND AREA OF VOLUMEOF SPHERE
+
+//const http = require('http');
+//const url = require('url');
+
+//http
+//.createServer(function (req, res) {
+//const queryObject = url.parse(req.url, true).query;
+//console.log(queryObject);
+
+//var pi = Number(queryObject.pi)
+//var r1 = Number(queryObject.r1) 
+//var r2 = Number(queryObject.r2) 
+
+
+//var test = pi*r1*r2
+
+//console.log(test)
+
+
+//res.writeHead(200, { 'Content-Type': 'text/html' });
+//res.end(JSON.stringify({ message: test }));
+//})
+//.listen(8080);
+
+
+
+
+//const http = require('http');
+//const url = require('url');
+
+//http
+  //.createServer(function (req, res) {
+    //const queryObject = url.parse(req.url, true).query;
+    //console.log(queryObject);
+
+    //var x = Number(queryObject.x)
+    //var pi = Number(queryObject.pi)
+    //var r1 = Number(queryObject.r1)
+    //var r2 = Number(queryObject.r2)
+    //var r3 = Number(queryObject.r3)
+
+
+
+    //var test = pi * r1 * r2 * r3
+
+    //console.log(test)
+
+
+    //res.writeHead(200, { 'Content-Type': 'text/html' });
+    //res.end(JSON.stringify({ message: test }));
+  //})
+  //.listen(8080);
+
+
+
+
+  //(4/3*3.1428*r*r*r)
